54faa67e1abd7b3b02478c12451aa7aa344746d1
Amit Kumar <amit.sharma55@gmail.com>
2026-03-22 18:29:52 -0500

    Use env var for table name and add error handling in Lambda

--- lambda/visitor_counter.py
+++ lambda/visitor_counter.py
@@ -1,34 +1,40 @@
 import json
+import os
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('c-r-c')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 def lambda_handler(event, context):
-    
-    response = table.get_item(
-        Key = {
-            'ID':'visits'
-        }
-    )
-    #print(response)
-    visit_count = response['Item']['counter'] 
-    visit_count = str(int(visit_count) + 1)
-    
-    response = table.put_item(
-        Item = {
-            'ID':'visits',
-            'counter': visit_count
-        }
-    )
+    try:
+        response = table.get_item(
+            Key={
+                'ID': 'visits'
+            }
+        )
+        visit_count = str(int(response['Item']['counter']) + 1)
 
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,OPTIONS,POST'
-        },
-        #'body': visit_count
-        'body': json.dumps({'visit_count': visit_count})
-    }
+        table.put_item(
+            Item={
+                'ID': 'visits',
+                'counter': visit_count
+            }
+        )
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,OPTIONS,POST'
+            },
+            'body': json.dumps({'visit_count': visit_count})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({'error': 'Internal server error'})
+        }
